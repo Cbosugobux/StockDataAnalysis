537774279ac61147edffabc39d971d42063098ae
Amy MIllimen-Tola <medaamy@gmail.com>
2025-02-13 21:59:57 -0500

    Updated Map Visualization Files
    Updated Map Visualization Files

--- MapVisualization.js
+++ MapVisualization.js
@@ -1,7 +1,7 @@
 // Creating the map object
 let myMap = L.map("map", {
     center: [40.7128, -74.0059],
-    zoom: 2
+    zoom: 2.5
 
   });
 
@@ -11,27 +11,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(myMap);
 
 // use D3 to load the data from the nyc.geojson file
-d3.json('companyLocation.geojson').then(
+d3.json('companyLocations.geojson').then(
     (data)=>{
         console.log(data);
 
-        // make a quick outline of the boroughs and neighborhoods 
-        //L.geoJson(data).addTo(myMap);
 
-        // use borough attribute to color the borough using this inline function
-        
-///        function boroughColor(borough)
-   ///     {
-      ///      // use borough name as a parameter to choose one of the following colors
-         ///   if (borough == "Brooklyn") return "yellow";
-            ///else if (borough == "Bronx") return "red";
-            ///else if (borough == "Manhattan") return "orange";
-            ///else if (borough == "Queens") return "green";
-            ///else if (borough == "Staten Island") return "purple";
-            ///else return "black";
-        ///}
-
-        // expand the properties of L.geoJson to style the boroughs
         L.geoJson(data,
             {
                 // use the style attribute to go and add some different styling to the outline
@@ -41,20 +25,21 @@ d3.json('companyLocation.geojson').then(
                     fillOpacity: 0.6,
                     weight: 2
                 }*/
-                style: function(feature)
-                {
-                    return {
-                        color: "black",
-                        fillColor: "blue",
-                        fillOpacity: 0.6,
-                        weight: 2
-                    }
-                },
+                //style: function(feature)
+                //{
+                    //return {
+                        //Text:{Name}
+                        //color: "black",
+                        //fillColor: "blue",
+                        //fillOpacity: 0.6,
+                        //weight: 2
+                    //}
+                //},
                 // onEachFeature property, can be used to tell what happens when we interact
                 // with the drawn out portions of the map (the boroughs and neighborhoods) 
                onEachFeature: function(feature, layer){
                 // Giving each feature a popup with information that's relevant to it
-                layer.bindPopup("<center><h2>" + feature.properties.neighborhood + "</h2> <hr> <h3>");
+                layer.bindPopup("<center><h2>" + feature.properties.Company + "<br>" + feature.properties.Ticker + "</h2> <hr> <h3>");
                 //set mouse events to change map styling when neighobrhoods are hovered
                  layer.on({
                                      mouseover: function(event)
@@ -62,20 +47,20 @@ d3.json('companyLocation.geojson').then(
                         // reference the item (layer) that triggers the event
                         layer = event.target;
                         // use setStyle to update the fillOpacity style property
-                        layer.setStyle({
-                            fillOpacity: 0.9,
-                            weight: 5
-                        });
+                        //layer.setStyle({
+                            //fillOpacity: 0.9,
+                            //weight: 5
+                        //});
                     },
                      mouseout: function(event)
                     {
                         // reference the item (layer) that triggers the event
-                        layer = event.target;
+                        //layer = event.target;
                         // use setStyle to update the fillOpacity style property
-                        layer.setStyle({
-                            fillOpacity: 0.6,
-                            weight: 2
-                        });
+                        //layer.setStyle({
+                           // fillOpacity: 0.6,
+                            //weight: 2
+                        //});
                     }
                 });
                }
