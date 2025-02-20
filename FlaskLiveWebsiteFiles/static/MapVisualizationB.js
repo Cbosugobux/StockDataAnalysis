fa352b7951cd53cb29eea56eb53cad2aa7fd6163
redfoot12 <robjowaisas@gmail.com>
2025-02-20 13:26:25 -0500

    Flaskified project push

---
+++ FlaskLiveWebsiteFiles/static/MapVisualizationB.js
@@ -0,0 +1,101 @@
+// Creating the map object
+let myMapB = L.map("mapB", {
+    center: [25, 10],
+    zoom: 1.5
+});
+
+// Adding the tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMapB);
+
+// Load the GeoJSON file
+fetch('http://127.0.0.1:5000/api/map')
+  .then(response => response.json())
+  .then(data => {
+
+    function dataColor(BidenChange) {
+        return BidenChange < 0 ? "tomato" : "limegreen";
+    }
+
+    function readiusSize(BidenChange) {
+        return BidenChange === 0 ? 1 : Math.abs(BidenChange) * 5;
+    }
+
+    function dataStyle(feature) {
+        return {
+            opacity: 0.5,
+            fillOpacity: 0.5,
+            fillColor: dataColor(feature.properties.BidenChange),
+            radius: readiusSize(feature.properties.BidenChange),
+            weight: 0.5,
+            stroke: true
+        };
+    }
+
+    // Function to show hover popup
+    function highlightFeature(event) {
+        let layer = event.target;
+
+        // Increase opacity on hover
+        layer.setStyle({
+            opacity: .75,
+            fillOpacity: .75
+        });
+
+        // Extract details from the GeoJSON properties
+        let company = layer.feature.properties.Company;
+        let ticker = layer.feature.properties.Ticker;
+        let stockChange = layer.feature.properties.BidenChange;
+        let city = layer.feature.properties.City || "Unknown";
+        let state = layer.feature.properties.State || "Unknown";
+        let country = layer.feature.properties.Country || "Unknown";
+
+        // Define popup content
+        let popupContent = `
+            <center>
+                <h2>${company} - ${ticker}</h2>
+                <hr>
+                <h3>Stock Change: ${stockChange}</h3>
+                <p><b>Location:</b> ${city}, ${state}, ${country}</p>
+            </center>`;
+
+        // Open popup with the new content
+        layer.bindPopup(popupContent).openPopup();
+    }
+
+    // Function to reset style and close popup on mouseout
+    function resetHighlight(event) {
+        let layer = event.target;
+        layer.setStyle({
+            opacity: 0.5,
+            fillOpacity: 0.5
+        });
+        layer.closePopup();
+    }
+
+    // Add the GeoJSON data
+    L.geoJson(data, {
+        pointToLayer: function (feature, latLng) {
+            return L.circleMarker(latLng);
+        },
+        style: dataStyle,
+        onEachFeature: function (feature, layer) {
+            // Default popup when clicking a marker
+            let defaultPopup = `
+                <center>
+                    <h2>${feature.properties.Company} - ${feature.properties.Ticker}</h2>
+                    <hr>
+                    <h3>Stock Change: ${feature.properties.BidenChange}</h3>
+                </center>`;
+
+            layer.bindPopup(defaultPopup);
+
+            // Add mouseover event to show the hover popup with City, State, and Country
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight
+            });
+        }
+    }).addTo(myMapB);
+});
